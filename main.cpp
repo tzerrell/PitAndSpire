dcf7f1cf92f223ad9c3cd0ee2b79300f147a6aa7
Tim Carrell <timncarrell@gmail.com>
2016-08-14 20:26:52 -0700

    Say "Hello World!"

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@
  */
 int main(int argc, char** argv) {
     Fl_Window *mainWindow = new Fl_Window(340, 180);
-    Fl_Box *box = new Fl_Box(20,40,300,100);
+    Fl_Box *box = new Fl_Box(20,40,300,100, "Hello World!");
     
     box->box(FL_UP_BOX);
     box->labelfont(FL_BOLD+FL_ITALIC);
